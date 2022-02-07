5809a3caea14d159d296906c01508842cdac9a7e
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-02-07 16:28:28 -0300

    configuring eureka clients

--- hr-worker/src/main/java/com/devms/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devms/hrworker/HrWorkerApplication.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.Arrays;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication implements CommandLineRunner {
 
