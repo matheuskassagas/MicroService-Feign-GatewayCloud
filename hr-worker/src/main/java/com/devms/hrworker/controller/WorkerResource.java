62928279907d3091870a27d0f1d6633aa640d7d5
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-02-09 11:11:47 -0300

    Gateway zuul

--- hr-worker/src/main/java/com/devms/hrworker/controller/WorkerResource.java
+++ hr-worker/src/main/java/com/devms/hrworker/controller/WorkerResource.java
@@ -34,11 +34,11 @@ public class WorkerResource {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> findById(@PathVariable Long id){
-        /*try{
+        try{
             Thread.sleep(3000L);
         }catch (InterruptedException e){
             e.printStackTrace();
-        }*/
+        }
 
         //imprime no logg o numero da porta que esta rodando
         Logger.info("PORT = " + env.getProperty("local.server.port"));
