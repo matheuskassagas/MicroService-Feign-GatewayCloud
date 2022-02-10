70c967e9863f106b8c7e2f1177674c0dde1b6d5e
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-02-10 15:47:03 -0300

    Segunda opção gateway cloud para fazer as requests

---
+++ hr-api-gateway-cloud/src/main/java/com/devms/hrapigatewaycloud/HrApiGatewayCloudApplication.java
@@ -0,0 +1,19 @@
+package com.devms.hrapigatewaycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class HrApiGatewayCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayCloudApplication.class, args);
+	}
+
+}
+
---
+++ hr-api-gateway-cloud/src/test/java/com/devms/hrapigatewaycloud/HrApiGatewayCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devms.hrapigatewaycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
