256b6e42e3ce09f0e2088e369d3933de1248ab2d
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-02-11 17:24:13 -0300

    user and role, many to many associate

--- hr-user/src/main/java/com/devms/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devms/hruser/HrUserApplication.java
@@ -1,15 +1,20 @@
 package com.devms.hruser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
---
+++ hr-user/src/main/java/com/devms/hruser/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.devms.hruser.repository;
+
+public interface UserRepository {
+}
